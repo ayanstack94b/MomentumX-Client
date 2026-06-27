c927c506e589d6c427f5432abf2d389008b052fa
unknown <ayondevs.94b@gmail.com>
2026-06-27 17:46:28 +0530

    updated new authentication

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -108,12 +108,13 @@ const LoginPage = () => {
         setIsLoading(true);
 
         try {
-            console.log(process.env.NEXT_PUBLIC_APP_URL);
+            console.log("APP URL:", process.env.NEXT_PUBLIC_APP_URL);
+
             await authClient.signIn.social({
                 provider: "google",
-                callbackURL: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`
+                callbackURL: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
             });
-           
+
         } catch (error) {
             console.error(error);
 
